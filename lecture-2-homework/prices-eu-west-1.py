3c278dca075e1f19d1e5bce5c5945203bbe0e4a5
g0shex <72013341+g0shex@users.noreply.github.com>
2020-12-05 09:18:16 +0200

    moved scripts to new folder

--- lecture-2-homework/prices-eu-west-1.py
+++ lecture-2-homework/prices-eu-west-1.py
@@ -5,6 +5,7 @@ api_base = 'https://pricing.us-east-1.amazonaws.com'
 api_one = 'https://pricing.us-east-1.amazonaws.com/offers/v1.0/aws/AmazonRDS/current/region_index.json'
 
 response = requests.get(api_one)
+
 j = response.json()
 
 res = (j['regions']['eu-west-1']['currentVersionUrl'])
@@ -14,6 +15,7 @@ final_url = api_base + res
 new = requests.get(final_url).json()
 
 on_demand = new['terms']['OnDemand']
+
 target = 'db.t3'
 
 for key, value in on_demand.items():
